29980ac8fe2151d518808d1a80a239e47277a364
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2018-09-27 14:18:56 +0100

    configure jest and previewer page test

--- src/Pages/PreviewerPage/PreviewerPage.jsx
+++ src/Pages/PreviewerPage/PreviewerPage.jsx
@@ -7,36 +7,34 @@ import logo from '../../assets/images/ms.png'
 
 import './PreviewerPage.scss'
 
-const PreviewerPage = ({ searchText, onSearchChange, movies }) => {
-  const msg = {
-    mainMsg: 'Please start your searching ...',
-    noMoviesMsg: 'Oops!! Nothing match your search'
-  }
-
-  return(
-    <div className="previewer-container">
-      <header className="previewer-header">
-        <img width="100" height="50" src={logo} alt="logo" />
-        <div className="search">
-          <SearchBar
-            val={searchText}
-            onChange={onSearchChange}
-          />
-        </div>
-      </header>
-      <main className="previewer-content">
-      {
-        searchText && movies.length > 0
-          ? movies.map(movie =>
-            <div key={movie.id} className="previewer-card">
-                <CardContainer {...movie} />
-            </div>
-          )
-          : <h1 className="previewer-message">{searchText ? msg.noMoviesMsg : msg.mainMsg}</h1>
-      }
-      </main>
-    </div>
-  )
+export const MSG = {
+  mainMsg: 'Please start your searching ...',
+  noMoviesMsg: 'Oops!! Nothing match your search'
 }
+
+const PreviewerPage = ({ searchText, onSearchChange, movies }) => (
+  <div className="previewer-container">
+    <header className="previewer-header">
+      <img className="logo" width="100" height="50" src={logo} alt="logo" />
+      <div className="search">
+        <SearchBar
+          val={searchText}
+          onChange={onSearchChange}
+        />
+      </div>
+    </header>
+    <main className="previewer-content">
+    {
+      searchText && movies.length > 0
+        ? movies.map(movie =>
+          <div key={movie.id} className="previewer-card">
+              <CardContainer {...movie} />
+          </div>
+        )
+        : <h1 className="previewer-message">{searchText ? MSG.noMoviesMsg : MSG.mainMsg}</h1>
+    }
+    </main>
+  </div>
+)
 
 export default PreviewerPage
--- src/Pages/PreviewerPage/PreviewerPage.spec.js
+++ src/Pages/PreviewerPage/PreviewerPage.spec.js
@@ -1,6 +1,66 @@
-import PreviwerPage from './PreviewerPage'
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import PreviwerPage, { MSG } from './PreviewerPage'
 
 describe('Previewer Page', () => {
-  it('should have a header', () => {
+  const props = {
+    movies: [],
+    onSearchChange: jest.fn(),
+    searchText: ''
+  }
+  describe('when trying to render', () => {
+    const component = shallow(<PreviwerPage {...props}/>)
+    it('should render the container', () => {
+      expect(component.find('.previewer-container').length).toBe(1)
+    })
+    it('should render a header', () => {
+      expect(component.find('.previewer-header').length).toBe(1)
+    })
+    it('should render the logo', () => {
+      expect(component.find('.logo').length).toBe(1)
+    })
+    it('should render the search bar with the search text and the onSearch handler', () => {
+      expect(component.find('.search').length).toBe(1)
+      expect(component.find('SearchBar').length).toBe(1)
+      expect(component.find('SearchBar').props()).toEqual({ val: props.searchText, onChange: props.onSearchChange })
+    })
+    it('should render the content tag', () => {
+      expect(component.find('.previewer-content').length).toBe(1)
+    })
+  })
+  describe('when trying to render the component', () => {
+    describe('and the user has not started the search', () => {
+      const component = shallow(<PreviwerPage {...props}/>)
+      it('should render the main message as a previewer message', () => {
+        const previewerMsg = component.find('.previewer-message')
+
+        expect(previewerMsg.text()).toBe(MSG.mainMsg)
+      })
+    })
+    describe('and the user has started the search', () => {
+      const newProps = {
+        ...props,
+        searchText: 'text'
+      }
+      describe('and not movies are feched', () => {
+        const component = shallow(<PreviwerPage {...newProps}/>)
+        it('should render the main message as a previewer message', () => {
+          const previewerMsg = component.find('.previewer-message')
+
+          expect(previewerMsg.text()).toBe(MSG.noMoviesMsg)
+        })
+      })
+      describe('and there are some movies feched', () => {
+        const currentProps = {
+          ...newProps,
+          movies: [{ name: 'movie' }, {name: 'second-movie'}]
+        }
+        const component = shallow(<PreviwerPage {...currentProps}/>)
+        it('will render as many card as movies received', () => {
+          expect(component.find('CardContainer').length).toBe(currentProps.movies.length)
+        })
+      })
+    })
   })
 })
